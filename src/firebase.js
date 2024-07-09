b30782e2a8bf2deb7f0701f4cec3602eb36918d9
Manish Goyal <90592341+manishgoyal07@users.noreply.github.com>
2024-07-09 17:55:24 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDz53ZPnWMx6Db7sGbc3V4UUJZSkE5WJyA",
-    authDomain: "loungify-272e4.firebaseapp.com",
-    projectId: "loungify-272e4",
-    storageBucket: "loungify-272e4.appspot.com",
-    messagingSenderId: "723666507902",
-    appId: "1:723666507902:web:ff7e85291c2022d8cba02f",
-    measurementId: "G-15HNLVYBRQ"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
   
   // Initialize Firebase
